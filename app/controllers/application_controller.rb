6225cd655696ebf0ffd967ebda846bf49e8e3eb3
zep1994 <snaxbags@protonmail.com>
2018-11-22 16:51:26 -0600

    update root path and some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-   request.env['omniauth-origin'] || appointments_path || root_path
+   request.env['omniauth-origin'] || root_path || appointments_path
   end
 end
